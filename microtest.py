85388b671a7d64442816f532aa414abd12165feb
bob <veltmeyer.s@gmail.com>
2026-06-20 10:25:58 -0700

    softmax

--- microtest.py
+++ microtest.py
@@ -86,6 +86,16 @@ class Value:
         out._backward = _backward
         return out
     
+    def log(self):
+        x = self.data
+        out = Value(math.log(x), (self,), 'log')
+
+        def _backward():
+            self.grad += (1/self.data) * out.grad
+
+        out._backward = _backward
+        return out
+    
     def backward(self):
     
         topo = []
@@ -172,6 +182,38 @@ for k in range(20):
     for p in n.parameters():
         p.data += -0.05 * p.grad
 
-    print(k, loss.data)
+    # print(k, loss.data)
 
-print(ypred)
+# print(ypred)
+
+
+
+
+
+
+
+# without referencing our code/video __too__ much, make this cell work
+# you'll have to implement (in some cases re-implemented) a number of functions
+# of the Value object, similar to what we've seen in the video.
+# instead of the squared error loss this implements the negative log likelihood
+# loss, which is very often used in classification.
+
+# this is the softmax function
+# https://en.wikipedia.org/wiki/Softmax_function
+def softmax(logits):
+  counts = [logit.exp() for logit in logits]
+  denominator = sum(counts)
+  out = [c / denominator for c in counts]
+  return out
+
+# this is the negative log likelihood loss function, pervasive in classification
+logits = [Value(0.0), Value(3.0), Value(-2.0), Value(1.0)]
+probs = softmax(logits)
+loss = -probs[3].log() # dim 3 acts as the label for this input example
+loss.backward()
+print(loss.data)
+
+ans = [0.041772570515350445, 0.8390245074625319, 0.005653302662216329, -0.8864503806400986]
+for dim in range(4):
+  ok = 'OK' if abs(logits[dim].grad - ans[dim]) < 1e-5 else 'WRONG!'
+  print(f"{ok} for dim {dim}: expected {ans[dim]}, yours returns {logits[dim].grad}")
